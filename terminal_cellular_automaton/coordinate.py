7c5f6e03d234a8fb948a44e19e61113de9626d34
noprobelm <noprobelm@protonmail.com>
2023-12-21 13:37:03 -0500

    Fixed type annotation

--- terminal_cellular_automaton/coordinate.py
+++ terminal_cellular_automaton/coordinate.py
@@ -17,10 +17,11 @@ class Coordinate:
         """Returns the sum of one coordinate and another. Primarily used to identify neighbors"""
         return Coordinate(self.x + other.x, self.y + other.y)
 
-    def __contains__(self, other: Coordinate) -> True:
+    def __contains__(self, other: Coordinate) -> bool:
         if 0 <= other.x <= self.x and 0 <= other.y <= self.y:
             return True
         return False
+
 
 class MooreNeighborhood(Enum):
     """Enumeration for variants of neighbors found in a Moore Neighborhood"""
