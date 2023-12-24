38869d3dd5882ed73ba597f88e5f2b865e6c68d5
noprobelm <noprobelm@protonmail.com>
2023-12-24 11:42:03 -0500

    Modified scenarios to support new changes

--- terminal_cellular_automaton/scenarios.py
+++ terminal_cellular_automaton/scenarios.py
@@ -11,84 +11,78 @@ Box = namedtuple("Box", ["top_left", "top_right", "bottom_left", "bottom_right"]
 
 
 def conway_1() -> Simulation:
-    sim = Simulation()
+    sim = Simulation[states.ConwayState](cell.MooreCell, states.ConwayState(False))
     for y in range(sim.ymax + 1):
         for x in range(sim.xmax + 1):
             alive = bool(random.randint(0, 1))
-            c = cell.MooreCell(
-                Coordinate(x, y),
-                sim.max_coord,
-                states.ConwayState(("green", "red"), alive),
-            )
-            sim.spawn(c)
+            coord = Coordinate(x, y)
+            s = states.ConwayState(alive)
+            sim.spawn(coord, s)
     return sim
 
 
 def conway_2() -> Simulation:
-    sim = Simulation()
+    sim = Simulation[states.ConwayState](cell.MooreCell, states.ConwayState(False))
     for y in range(sim.ymax + 1):
         for x in range(sim.xmax + 1):
+            coord = Coordinate(x, y)
             r = random.randint(0, 10)
             if r > 9:
                 alive = True
             else:
                 alive = False
-            c = cell.MooreCell(
-                Coordinate(x, y),
-                sim.max_coord,
-                states.ConwayState(("green", "red"), alive),
-            )
-            sim.spawn(c)
+            s = states.ConwayState(alive)
+            sim.spawn(coord, s)
     return sim
 
 
 def pulsar() -> Simulation:
-    sim = Simulation()
+    sim = Simulation[states.ConwayState](cell.MooreCell, states.ConwayState(False))
     for y in range(sim.ymax + 1):
         for x in range(sim.xmax + 1):
-            c = cell.MooreCell(
-                Coordinate(x, y),
-                sim.max_coord,
-                states.ConwayState(("green", "red"), False),
-            )
-            sim.spawn(c)
+            coord = Coordinate(x, y)
+            s = states.ConwayState(alive=False)
+            sim.spawn(coord, s)
 
     midpoint = Coordinate(sim.xmax // 2, sim.ymax // 2)
     top_left = Coordinate(midpoint.x - 7, midpoint.y - 7)
-    state = states.ConwayState(("green", "red"), True)
+    state = states.ConwayState(alive=True)
     row = top_left + Coordinate(0, 3)
     for x in range(4, 7):
-        c = cell.MooreCell(row + Coordinate(x, 0), sim.max_coord, state)
-        sim.spawn(c)
-        c = cell.MooreCell(row + Coordinate(x + 6, 0), sim.max_coord, state)
-        sim.spawn(c)
+        coord = row + Coordinate(x, 0)
+        sim.spawn(coord, state)
+
+        coord = row + Coordinate(x + 6, 0)
+        sim.spawn(coord, state)
 
     row = top_left + Coordinate(0, 8)
     for x in range(4, 7):
-        c = cell.MooreCell(row + Coordinate(x, 0), sim.max_coord, state)
-        sim.spawn(c)
-        c = cell.MooreCell(row + Coordinate(x + 6, 0), sim.max_coord, state)
-        sim.spawn(c)
+        coord = row + Coordinate(x, 0)
+        sim.spawn(coord, state)
+
+        coord = row + Coordinate(x + 6, 0)
+        sim.spawn(coord, state)
 
     row = top_left + Coordinate(0, 10)
     for x in range(4, 7):
-        c = cell.MooreCell(row + Coordinate(x, 0), sim.max_coord, state)
-        sim.spawn(c)
-        c = cell.MooreCell(row + Coordinate(x + 6, 0), sim.max_coord, state)
-        sim.spawn(c)
+        coord = row + Coordinate(x, 0)
+        sim.spawn(coord, state)
+        coord = row + Coordinate(x + 6, 0)
+        sim.spawn(coord, state)
 
     row = top_left + Coordinate(0, 15)
     for x in range(4, 7):
-        c = cell.MooreCell(row + Coordinate(x, 0), sim.max_coord, state)
-        sim.spawn(c)
-        c = cell.MooreCell(row + Coordinate(x + 6, 0), sim.max_coord, state)
-        sim.spawn(c)
+        coord = row + Coordinate(x, 0)
+        sim.spawn(coord, state)
+
+        coord = row + Coordinate(x + 6, 0)
+        sim.spawn(coord, state)
 
     for y in [5, 6, 7, 11, 12, 13]:
         for x in (2, 7, 9, 14):
             for _ in range(4):
-                c = cell.MooreCell(top_left + Coordinate(x, y), sim.max_coord, state)
-                sim.spawn(c)
+                coord = top_left + Coordinate(x, y)
+                sim.spawn(coord, state)
 
     return sim
 
