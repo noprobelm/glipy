bb75eeaf0d2a8faee65ab1984236ff1ff63b69b5
noprobelm <noprobelm@protonmail.com>
2023-12-24 11:41:33 -0500

    Removed MooreNeighborhood from Coordinate module

--- terminal_cellular_automaton/coordinate.py
+++ terminal_cellular_automaton/coordinate.py
@@ -21,16 +21,3 @@ class Coordinate:
         if 0 <= other.x <= self.x and 0 <= other.y <= self.y:
             return True
         return False
-
-
-class MooreNeighborhood(Enum):
-    """Enumeration for variants of neighbors found in a Moore Neighborhood"""
-
-    UPPER_LEFT = Coordinate(-1, -1)
-    UPPER = Coordinate(0, -1)
-    UPPER_RIGHT = Coordinate(1, -1)
-    RIGHT = Coordinate(1, 0)
-    LOWER_RIGHT = Coordinate(1, 1)
-    LOWER = Coordinate(0, 1)
-    LOWER_LEFT = Coordinate(-1, 1)
-    LEFT = Coordinate(-1, 0)
