9f5da1e8811c8bd2dad876dee69f08be3a8d27b1
noprobelm <noprobelm@protonmail.com>
2023-12-30 16:04:52 -0500

    Added generics for Automaton and renamed some vars
    - Added 'C' (Cell) and 'S' (CellState) TypeVar and Generics to Automaton
    class signature
    - Added corresponding tpye hints for generics in scenarios module
    - Renamed 'sim' to 'automaton' everywhere in scenarios module

--- terminal_cellular_automaton/scenarios.py
+++ terminal_cellular_automaton/scenarios.py
@@ -16,21 +16,21 @@ from .state import ConwayState
 
 def conway_1() -> Automaton:
     """A random game, where each cell has a 1 in 2 chance of spawning as alive"""
-    sim = Automaton(MooreCell, ConwayState(False))
-    for y in range(sim.ymax + 1):
-        for x in range(sim.xmax + 1):
+    automaton = Automaton[MooreCell, ConwayState](MooreCell, ConwayState(False))
+    for y in range(automaton.ymax + 1):
+        for x in range(automaton.xmax + 1):
             alive = bool(random.randint(0, 1))
             coord = Coordinate(x, y)
             s = ConwayState(alive)
-            sim.set_state(coord, s)
-    return sim
+            automaton.set_state(coord, s)
+    return automaton
 
 
 def conway_2() -> Automaton:
     """A random game, where each cell has a 1 in 10 chance of spawning as alive"""
-    sim = Automaton(MooreCell, ConwayState(False))
-    for y in range(sim.ymax + 1):
-        for x in range(sim.xmax + 1):
+    automaton = Automaton[MooreCell, ConwayState](MooreCell, ConwayState(False))
+    for y in range(automaton.ymax + 1):
+        for x in range(automaton.xmax + 1):
             coord = Coordinate(x, y)
             r = random.randint(0, 10)
             if r > 9:
@@ -38,74 +38,74 @@ def conway_2() -> Automaton:
             else:
                 alive = False
             s = ConwayState(alive)
-            sim.set_state(coord, s)
-    return sim
+            automaton.set_state(coord, s)
+    return automaton
 
 
 def pulsar() -> Automaton:
     """A pulsar life generated from the patterns module"""
-    sim = Automaton(MooreCell, ConwayState(False))
+    automaton = Automaton[MooreCell, ConwayState](MooreCell, ConwayState(False))
     pulsar = patterns.Pulsar()
-    sim.spawn(sim.midpoint - pulsar.midpoint, pulsar)
-    return sim
+    automaton.spawn(automaton.midpoint - pulsar.midpoint, pulsar)
+    return automaton
 
 
 def glider() -> Automaton:
     """A glider life generated from the patterns module"""
-    sim = Automaton(MooreCell, ConwayState(False))
+    automaton = Automaton[MooreCell, ConwayState](MooreCell, ConwayState(False))
     glider = patterns.Glider()
-    sim.spawn(glider.midpoint, glider)
-    return sim
+    automaton.spawn(glider.midpoint, glider)
+    return automaton
 
 
 def clover_leaf() -> Automaton:
     """A clover leaf generated from the patterns module"""
-    sim = Automaton(MooreCell, ConwayState(False))
+    automaton = Automaton[MooreCell, ConwayState](MooreCell, ConwayState(False))
     leaf = patterns.CloverLeaf()
-    sim.spawn(sim.midpoint - leaf.midpoint, leaf)
-    return sim
+    automaton.spawn(automaton.midpoint - leaf.midpoint, leaf)
+    return automaton
 
 
 def domino_sparker() -> Automaton:
     """A domino sparker generated from an rle file"""
-    sim = Automaton(MooreCell, ConwayState(False))
+    automaton = Automaton[MooreCell, ConwayState](MooreCell, ConwayState(False))
     rle = os.path.join(Path(__file__).parent, "data/rle/p11dominosparkeron56p27.rle")
     with open(rle, "r") as f:
         lines = f.readlines()
     sparker = patterns.ConwayPattern.from_rle(lines)
-    sim.spawn(sim.midpoint - sparker.midpoint, sparker)
-    return sim
+    automaton.spawn(automaton.midpoint - sparker.midpoint, sparker)
+    return automaton
 
 
 def from_life(path: str) -> Automaton:
     """Runs a .life from a local path"""
-    sim = Automaton(MooreCell, ConwayState(False))
+    automaton = Automaton[MooreCell, ConwayState](MooreCell, ConwayState(False))
     with open(path, "r") as f:
         lines = f.readlines()
     pattern = patterns.ConwayPattern.from_life(lines)
-    sim.spawn(sim.midpoint - pattern.midpoint, pattern)
-    return sim
+    automaton.spawn(automaton.midpoint - pattern.midpoint, pattern)
+    return automaton
 
 
 def from_rle(path: str) -> Automaton:
     """Runs a .rle from a local path"""
-    sim = Automaton(MooreCell, ConwayState(False))
+    automaton = Automaton[MooreCell, ConwayState](MooreCell, ConwayState(False))
     with open(path, "r") as f:
         lines = f.readlines()
     pattern = patterns.ConwayPattern.from_rle(lines)
-    sim.spawn(sim.midpoint - pattern.midpoint, pattern)
-    return sim
+    automaton.spawn(automaton.midpoint - pattern.midpoint, pattern)
+    return automaton
 
 
 def from_url(url: str) -> Automaton:
     """Runs a .rle from a remote URL"""
-    sim = Automaton(MooreCell, ConwayState(False))
+    automaton = Automaton[MooreCell, ConwayState](MooreCell, ConwayState(False))
     response = requests.get(url)
     if response.status_code != 200:
         raise ValueError(f"Error {response.status_code}: {response.reason}")
     data = response.content.decode()
     lines = data.strip().split("\n")
     pattern = patterns.ConwayPattern.from_rle(lines)
-    sim.spawn(sim.midpoint - pattern.midpoint, pattern)
+    automaton.spawn(automaton.midpoint - pattern.midpoint, pattern)
 
-    return sim
+    return automaton
