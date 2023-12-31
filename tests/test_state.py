8b45e34c211ded6955dba162f5068b736651b531
noprobelm <noprobelm@protonmail.com>
2023-12-31 14:50:46 -0500

    Added tests for colors interface

--- tests/test_state.py
+++ tests/test_state.py
@@ -58,3 +58,38 @@ def _():
 def _():
     s = ConwayState(False)
     assert s.color == s.colors[1]
+
+
+@test(
+    "Colors Interface: The default colors of a CellState will be overriden when new colors are provided to the setter"
+)
+def _():
+    s = ConwayState(True)
+    colors = ["blue", "yellow"]
+    s.colors = colors
+    assert s.colors == colors
+    assert len(s.colors) == 2
+
+
+@test(
+    "Colors Interface: Providing fewer colors than the number of states will result in a CellState right padding with \
+    existing colors"
+)
+def _():
+    s = ConwayState(True)
+    old = s.colors
+    colors = ["blue"]
+    s.colors = colors
+    assert s.colors == ["blue", old[1]]
+    assert len(s.colors) == 2
+
+
+@test(
+    "Colors Interface: Providing more colors than the number of states will result in a the extra colors being dropped"
+)
+def _():
+    s = ConwayState(True)
+    colors = ["blue", "green", "yellow"]
+    s.colors = colors
+    assert s.colors == ["blue", "green"]
+    assert len(s.colors) == 2
