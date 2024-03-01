7d4de00de43e839b751b99d8070336ce200e1226
noprobelm <noprobelm@protonmail.com>
2024-03-01 08:59:58 -0500

    Wrote tests for Neuman Cell, also modified logic for Moore tests
    Implemented all() function to check for neighbors instead of
    individual assert

--- tests/test_cell.py
+++ tests/test_cell.py
@@ -1,7 +1,7 @@
 """Tests the get_neighbors method for all Cell types"""
 
 from ward import test, fixture
-from glipy.cell import MooreCell
+from glipy.cell import MooreCell, NeumannCell
 from glipy.coordinate import Coordinate
 
 
@@ -33,17 +33,19 @@ def _():
 def _():
     c = MooreCell(Coordinate(0, 0))
     neighbors = c.get_neighbors(max_coord())
-    for n in [
-        Coordinate(2, 2),
-        Coordinate(0, 2),
-        Coordinate(2, 0),
-        Coordinate(1, 2),
-        Coordinate(1, 0),
-        Coordinate(1, 1),
-        Coordinate(0, 1),
-        Coordinate(2, 1),
-    ]:
-        assert n in neighbors
+    assert all(
+        n in neighbors
+        for n in [
+            Coordinate(2, 2),
+            Coordinate(0, 2),
+            Coordinate(2, 0),
+            Coordinate(1, 2),
+            Coordinate(1, 0),
+            Coordinate(1, 1),
+            Coordinate(0, 1),
+            Coordinate(2, 1),
+        ]
+    )
 
 
 @test(
@@ -52,13 +54,33 @@ def _():
 def _():
     c = MooreCell(Coordinate(2, 2))
     neighbors = c.get_neighbors(max_coord())
-    for n in [
-        Coordinate(2, 1),
-        Coordinate(0, 1),
-        Coordinate(0, 2),
-        Coordinate(0, 0),
-        Coordinate(2, 0),
-        Coordinate(1, 0),
-        Coordinate(1, 2),
-    ]:
-        assert n in neighbors
+    assert all(
+        n in neighbors
+        for n in [
+            Coordinate(2, 1),
+            Coordinate(0, 1),
+            Coordinate(0, 2),
+            Coordinate(0, 0),
+            Coordinate(2, 0),
+            Coordinate(1, 0),
+            Coordinate(1, 2),
+        ]
+    )
+
+
+@test(
+    "NeumannCell: A cell located at the top left of a matrix will have neighbors that wrap to the other side"
+)
+def _():
+    c = NeumannCell(Coordinate(0, 0))
+    neighbors = c.get_neighbors(max_coord())
+    assert all(n in neighbors for n in [Coordinate(0, 2), Coordinate(2, 0)])
+
+
+@test(
+    "NeumannCell: A cell located at the bottom right of a matrix will have neighbors that wrap to the other side"
+)
+def _():
+    c = MooreCell(Coordinate(2, 2))
+    neighbors = c.get_neighbors(max_coord())
+    assert all(n in neighbors for n in [Coordinate(2, 0), Coordinate(0, 2)])
