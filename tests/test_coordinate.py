a3184f5c2e41fc1c848f4989504a283616ba15a3
Jeff Barfield <jeffreybarfield@protonmail.com>
2025-05-13 23:23:45 -0400

    Fixe line lengths for test_coordinate module

--- tests/test_coordinate.py
+++ tests/test_coordinate.py
@@ -6,22 +6,24 @@ from glipy.coordinate import Coordinate
 
 
 @test(
-    "Adding 2 coordinates will result in a new coordinate whose x/y values are the sum of the others",
+    "Adding 2 coordinates will result in a new coordinate whose x/y values are the sum of the \
+    others",
 )
 def _() -> None:
     assert Coordinate(1, 1) + Coordinate(1, 1) == Coordinate(2, 2)
 
 
 @test(
-    "Subtracting 2 coordinates will result in a new coordinate whose x/y values are the difference of the others",
+    "Subtracting 2 coordinates will result in a new coordinate whose x/y values are the \
+    difference of the others",
 )
 def _() -> None:
     assert Coordinate(1, 1) - Coordinate(1, 1) == Coordinate(0, 0)
 
 
 @test(
-    "A coordinate is within another coordinate if its x and y values are greater than 0 and less than the coordinte \
-being compared against",
+    "A coordinate is within another coordinate if its x and y values are greater than 0 and less \
+    than the coordinte being compared against",
 )
 def _() -> None:
     assert Coordinate(0, 0) in Coordinate(1, 1)
