83b8c7c3758aca6bbeb0578d418e31b41af7bf74
Jeff Barfield <jeffreybarfield@protonmail.com>
2025-05-13 23:41:01 -0400

    Ruff fixes to coordinate module

--- glipy/coordinate.py
+++ glipy/coordinate.py
@@ -13,12 +13,13 @@ class Coordinate:
     y: int
 
     def __add__(self, other: Coordinate) -> Coordinate:
-        """Returns the sum of one coordinate and another. Primarily used to identify neighbors."""
+        """Return the sum of one coordinate and another. Primarily used to identify neighbors."""
         return Coordinate(self.x + other.x, self.y + other.y)
 
     def __sub__(self, other: Coordinate) -> Coordinate:
-        """Returns the sum of one coordinate and another. Primarily used to identify neighbors."""
+        """Return the sum of one coordinate and another. Primarily used to identify neighbors."""
         return Coordinate(self.x - other.x, self.y - other.y)
 
     def __contains__(self, other: Coordinate) -> bool:
+        """Return whether the coordinate is greater than another coordinate"""
         return bool(0 <= other.x <= self.x and 0 <= other.y <= self.y)
