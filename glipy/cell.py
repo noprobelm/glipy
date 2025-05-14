ba7b3a3a85e6c2c243dd82b6bd03a540eab61a48
Jeff Barfield <jeffreybarfield@protonmail.com>
2025-05-13 23:53:11 -0400

    Apply ruff fixes to cell module

--- glipy/cell.py
+++ glipy/cell.py
@@ -1,3 +1,5 @@
+"""Contains classes and functionality for cells."""
+
 from typing import Protocol
 
 from .coordinate import Coordinate
@@ -6,13 +8,14 @@ from .coordinate import Coordinate
 class Cell(Protocol):
     """A protocol to reference when creating a new type of cell.
 
-    A cell is primarily defined by its neighbors. A class that conforms to this protocol shoud have an __init__ method
-    which accepts a coordinate, and a get_neighbors method that will retreive valid neighbors based on a maximum
-    possible coordinate
+    A cell is primarily defined by its neighbors. A class that conforms to this protocol shoud have
+    an __init__ method which accepts a coordinate, and a get_neighbors method that will retreive
+    valid neighbors based on a maximum possible coordinate
 
     Attributes:
         coord (Coordinate): The coordinate of a given cell
-        neighbors (Tuple[Coordinate, ...]): All valid neighbors of a cell based on the coordinate attribute
+        neighbors (Tuple[Coordinate, ...]): All valid neighbors of a cell based on the coordinate
+        attribute
 
     """
 
@@ -20,17 +23,17 @@ class Cell(Protocol):
     neighbors: tuple[Coordinate, ...]
 
     def __init__(self, coord: Coordinate) -> None:
-        """Initializes an instance of a cell. The coord attr should be set here."""
+        """Initialize an instance of a cell. The coord attr should be set here."""
 
     def get_neighbors(self, max_coord: Coordinate) -> list[Coordinate]:
-        """Accesses members of a cell's neighborhood and returns a list of valid neighbors.
+        """Access members of a cell's neighborhood and returns a list of valid neighbors.
 
         This is usually achieved by checking coordinates against a maximum possible coordinate. The
         Coordinate.__contains__ special method is available for determining neighbors.
 
         Args:
-            max_coord (Coordinate): The maximum possible coordinate of a neighbor (usually the max coord of the
-                underlying matrix)
+            max_coord (Coordinate): The maximum possible coordinate of a neighbor (usually the max
+            coord of the underlying matrix)
 
         Returns:
             A list of all valid neighbors
@@ -71,16 +74,16 @@ class MooreCell:
     )
 
     def __init__(self, coord: Coordinate) -> None:
-        """Initializes an instance of the MooreCell class."""
+        """Initialize an instance of the MooreCell class."""
         self.coord = coord
 
     def get_neighbors(self, max_coord: Coordinate) -> list[Coordinate]:
-        """Gets neighbors based on the max coord.
+        """Get neighbors based on the max coord.
 
-        Neighbors will usually be the eight surrounding cells in an automaton, but for cells living along the min/max
-        coords, neighbors will wrap around to the other side of this grid. This ensures continuity and enables
-        a life to wrap around the other side of the simulation once it reaches a boundary, emulating a pseudo-infinite
-        space.
+        Neighbors will usually be the eight surrounding cells in an automaton, but for cells living
+        along the min/max coords, neighbors will wrap around to the other side of this grid.
+        This ensures continuity and enables a life to wrap around the other side of the simulation
+        once it reaches a boundary, emulating a pseudo-infinite space.
 
         Args:
             max_coord (Coordinate): The maximum coordinate found in the underlying Automaton
@@ -140,16 +143,16 @@ class NeumannCell:
     )
 
     def __init__(self, coord: Coordinate) -> None:
-        """Initializes an instance of the MooreCell class."""
+        """Initialize an instance of the MooreCell class."""
         self.coord = coord
 
     def get_neighbors(self, max_coord: Coordinate) -> list[Coordinate]:
-        """Gets neighbors based on the max coord.
+        """Get neighbors based on the max coord.
 
-        Neighbors will usually be the eight surrounding cells in an automaton, but for cells living along the min/max
-        coords, neighbors will wrap around to the other side of this grid. This ensures continuity and enables
-        a life to wrap around the other side of the simulation once it reaches a boundary, emulating a pseudo-infinite
-        space.
+        Neighbors will usually be the eight surrounding cells in an automaton, but for cells living
+        along the min/max coords, neighbors will wrap around to the other side of this grid. This
+        ensures continuity and enables a life to wrap around the other side of the simulation
+        once it reaches a boundary, emulating a pseudo-infinite space.
 
         Args:
             max_coord (Coordinate): The maximum coordinate found in the underlying Automaton
