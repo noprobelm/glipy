e5c4a7fbfc920e6f83e5ab960bf7fe6d06bd62ac
Jeff Barfield <jeffreybarfield@protonmail.com>
2025-05-13 23:39:29 -0400

    Apply ruff fixes to state module

--- glipy/state.py
+++ glipy/state.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Protocol, Self
+from typing import TYPE_CHECKING, ClassVar, Protocol, Self
 
 from .color import Color
 
@@ -11,24 +11,26 @@ if TYPE_CHECKING:
 class CellState(Protocol):
     """A protocol to reference when creating a new type of cell state."""
 
-    # colors should be a tuple of colors equivalent to the number of possible states in a CellState class
+    # colors should be a tuple of colors equivalent to the number of possible states in a CellState
+    # class
     colors: Sequence[Color]
 
     @property
     def color(self) -> Color:
-        """Correlates a color with arbitrary attributes of a CellState instance.
+        """Correlate a color with arbitrary attributes of a CellState instance.
 
         Returns:
-            A valid color designator (can be 'red', hex codes, etc. See the rich documentation for details)
+            A valid color designator (can be 'red', hex codes, etc. See the rich documentation
+            for details)
 
         """
 
     @classmethod
     def set_colors(cls, colors: list[Color]) -> None:
-        """Sets the colors for the CellState."""
+        """Set the colors for the CellState."""
 
     def change_state(self, neighbors: list[Self]) -> CellState:
-        """Retrieves a new state based on its neighbors.
+        """Retrieve a new state based on its neighbors.
 
         Returns:
             The cell's new state
@@ -40,27 +42,29 @@ class ConwayState:
     """A state that follows the rules for Conway's Game of Life.
 
     Attributes:
-        colors (Tuple[Color, Color]): A tuple of colors (see Textualize's documentation for 'rich' for accepted values).
-                                      The first index is the color for ALIVE states. 2nd is DEAD
-        birth_rules (List[int]): A list of integers representing the rules for a cell to become "resurrect"
-        survival_rules (List[int]): A list of integers representing the rules for a cell to stay alive
+        colors (Tuple[Color, Color]): A tuple of colors (see Textualize's documentation for 'rich'
+        for accepted values). The first index is the color for ALIVE states. 2nd is DEAD
+        birth_rules (list[int]): A list of integers representing the rules for a cell to become
+        "resurrected"
+        survival_rules (list[int]): A list of integers representing the rules for a cell to stay
+        alive
         alive (bool): Flag for whether the cell is ALIVE (True) or DEAD (False)
 
     """
 
     colors: Sequence[Color] = [Color("#F6AE2D"), Color("#315771")]
-    birth_rules = [3]
-    survival_rules = [2, 3]
+    birth_rules: ClassVar[list[int]] = [3]
+    survival_rules: ClassVar[list[int]] = [2, 3]
 
     def __init__(
         self,
         alive: bool = False,
     ) -> None:
-        """Initializes an instance of the ConwayState class.
+        """Initialize an instance of the ConwayState class.
 
         Args:
-            colors (Tuple[Color, Color]): A tuple of colors (see Textualize's documentation for 'rich' for accepted values).
-                                      The first index is the color for ALIVE states. 2nd is DEAD
+            colors (Tuple[Color, Color]): A tuple of colors (see Textualize's documentation for
+            'rich' for accepted values). The first index is the color for ALIVE states. 2nd is DEAD
             alive (bool): Flag for whether the cell is ALIVE (True) or DEAD (False)
 
         """
@@ -68,14 +72,14 @@ class ConwayState:
 
     @property
     def color(self) -> Color:
-        """Returns the first index of self.colors if alive, else the second."""
+        """Return the first index of self.colors if alive, else the second."""
         if self.alive is True:
             return self.colors[0]
         return self.colors[1]
 
     @classmethod
     def set_colors(cls, colors: list[Color]) -> None:
-        """Sets the colors for the CellState.
+        """Set the colors for the CellState.
 
         Args:
             colors (List[Color]): The list of colors to change to
@@ -89,10 +93,11 @@ class ConwayState:
         cls.colors = colors
 
     def change_state(self, neighbors: list[ConwayState]) -> ConwayState:
-        """Changes the state of the cell.
+        """Change the state of the cell.
 
-        An instance of this class will follow birth/survival rules according to the ConwayState.birth_rules and
-        ConwayState.survival_rules class attributes. Default rules are B3/S23 in accordance with the common standard
+        An instance of this class will follow birth/survival rules according to the
+        ConwayState.birth_rules and ConwayState.survival_rules class attributes. Default rules are
+        B3/S23.
 
         Args:
             neighbors (List[ConwayState]): A list of neighbor's states
@@ -109,10 +114,10 @@ class ConwayState:
         match self.alive:
             case True:
                 if any(alive_count == r for r in self.survival_rules):
-                    return ConwayState(True)
-                return ConwayState(False)
+                    return ConwayState(alive=True)
+                return ConwayState(alive=False)
 
             case False:
                 if any(alive_count == r for r in self.birth_rules):
-                    return ConwayState(True)
-                return ConwayState(False)
+                    return ConwayState(alive=True)
+                return ConwayState(alive=False)
